2fc7641ec1ff51dfdea1798ed41a4be181c7b037
Rinat <turkmengali.rinat@icloud.com>
2026-01-24 09:17:37 +0500

    Project

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,4 +1,4 @@
-static int count = 0;
+
 
 void main() {
     Scanner scanner = new Scanner(System.in);
@@ -7,143 +7,140 @@ void main() {
     RentalService rentalService = new RentalService("City Car Rental");
 
     // 2. Добавляем ввод имени клиента с клавиатуры
-    IO.println("Enter your name:");
+    System.out.println("Enter your name:");
     String userName = scanner.nextLine(); // Программа будет ждать, пока ты введешь имя
     Client client = new Client(101, userName);
 
-    IO.println("Hello, " + client.getName() + "! Welcome to " + rentalService.getCompanyName());
+    System.out.println("Hello, " + client.getName() + "! Welcome to " + rentalService.getCompanyName());
 
     // Машины
     // эконом
-    Vehicle eco1 = new Vehicle(1, "Toyota", "Corolla", 100, "economy");
-    Vehicle eco2 = new Vehicle(2, "Hyunday", "Accent", 150, "economy");
-    Vehicle eco3 = new Vehicle(3, "Chevrolet", "monza", 200, "economy");
+    Vehicle eco1 = new Vehicle(1, "Toyota", "Corolla", 100, "economy", 1);
+    Vehicle eco2 = new Vehicle(2, "Hyunday", "Accent", 150, "economy", 1);
+    Vehicle eco3 = new Vehicle(3, "Chevrolet", "monza", 200, "economy", 1);
     // спорт
-    Vehicle sport1 = new Vehicle(4, "BMW", "M4", 300, "sport");
-    Vehicle sport2 = new Vehicle(5, "AUDI", "RS7", 400, "sport");
-    Vehicle sport3 = new Vehicle(6, "Porshe", "911", 500, "sport");
+    Vehicle sport1 = new Vehicle(4, "BMW", "M4", 300, "sport", 2);
+    Vehicle sport2 = new Vehicle(5, "AUDI", "RS7", 400, "sport", 2);
+    Vehicle sport3 = new Vehicle(6, "Porshe", "911", 500, "sport", 2);
     // премиум
-    Vehicle prem1 = new Vehicle(7, "Mercedes", "S-Class", 600, "premium");
-    Vehicle prem2 = new Vehicle(8, "Lexus", "LS", 550, "premium");
-    Vehicle prem3 = new Vehicle(9, "Rolse Royse", "Phantom", 1000, "premium");
+    Vehicle prem1 = new Vehicle(7, "Mercedes", "S-Class", 600, "premium", 3);
+    Vehicle prem2 = new Vehicle(8, "Lexus", "LS", 550, "premium", 3);
+    Vehicle prem3 = new Vehicle(9, "Rolse Royse", "Phantom", 1000, "premium", 3);
 
     // 2. Сравнение объектов перед началом работы (Пункт 10)
-    IO.println("--- System Check: Comparing car prices ---");
+    System.out.println("--- System Check: Comparing car prices ---");
     if (eco1.getId() != eco2.getId()) {
-        IO.println("Comparison: " + eco1.brand + " and " + eco2.brand + " are different objects.");
+        System.out.println("Comparison: " + eco1.brand + " and " + eco2.brand + " are different objects.");
     }
-    IO.println("------------------------------------------\n");
+    System.out.println("------------------------------------------\n");
 
-    IO.println("Welcome to " + rentalService.getCompanyName());
-    IO.println("Client: " + client.getName());
+    System.out.println("Welcome to " + rentalService.getCompanyName());
+    System.out.println("Client: " + client.getName());
 
     // Выбор категории
-    IO.println("Choose category:");
-    IO.println("1 - Economy");
-    IO.println("2 - Sport");
-    IO.println("3 - Premium");
+    System.out.println("Choose category:");
+    System.out.println("1 - Economy");
+    System.out.println("2 - Sport");
+    System.out.println("3 - Premium");
 
     int categoryChoice = scanner.nextInt();
 
     // Показ машин выбранной категории
     if (categoryChoice == 1) {
-        IO.println("Economy cars:");
-        IO.println(eco1);
-        IO.println(eco2);
-        IO.println(eco3);
+        System.out.println("Economy cars:");
+        System.out.println(eco1);
+        System.out.println(eco2);
+        System.out.println(eco3);
     } else if (categoryChoice == 2) {
-        IO.println("Sport cars:");
-        IO.println(sport1);
-        IO.println(sport2);
-        IO.println(sport3);
+        System.out.println("Sport cars:");
+        System.out.println(sport1);
+        System.out.println(sport2);
+        System.out.println(sport3);
     } else if (categoryChoice == 3) {
-        IO.println("Premium cars:");
-        IO.println(prem1);
-        IO.println(prem2);
-        IO.println(prem3);
+        System.out.println("Premium cars:");
+        System.out.println(prem1);
+        System.out.println(prem2);
+        System.out.println(prem3);
     } else {
-        IO.println("Wrong category");
+        System.out.println("Wrong category");
         return;
     }
 
     // Выбор машины
-    IO.println("Choose car id:");
+    System.out.println("Choose car id:");
     int carChoice = scanner.nextInt();
 
-    if (carChoice <= 3 && carChoice >= 1) {
-        if (carChoice == eco1.getId()) {
-            if (eco1.isAvailable()) {
-                eco1.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + eco1);
-            } else {
-                IO.println("This car is already rented");
-            }
-        } else if (carChoice == eco2.getId()) {
+    if (carChoice == eco1.getId()) {
+        if (eco1.isAvailable()) {
+            eco1.setAvailable(false);
+            System.out.println(client.getName() + ", you rented: " + eco1);
+        }
+        else {
+            System.out.println("This car is already rented");
+        }
+    }
+    else if (carChoice == eco2.getId()) {
             if (eco2.isAvailable()) {
                 eco2.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + eco2);
+                System.out.println(client.getName() + ", you rented: " + eco2);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
         } else if (carChoice == eco3.getId()) {
             if (eco3.isAvailable()) {
                 eco3.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + eco3);
+                System.out.println(client.getName() + ", you rented: " + eco3);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
-        }
     }
-    if (carChoice > 3 && carChoice <= 6) {
+
         if (carChoice == sport1.getId()) {
             if (sport1.isAvailable()) {
                 sport1.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + sport1);
+                System.out.println(client.getName() + ", you rented: " + sport1);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
         } else if (carChoice == sport2.getId()) {
             if (sport2.isAvailable()) {
                 sport2.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + sport2);
+                System.out.println(client.getName() + ", you rented: " + sport2);
             } else {
                 IO.println("This car is already rented");
             }
         } else if (carChoice == sport3.getId()) {
             if (sport3.isAvailable()) {
                 sport3.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + sport3);
+                System.out.println(client.getName() + ", you rented: " + sport3);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
         }
-    }
-    if (carChoice > 6 && carChoice <= 9) {
+
         if (carChoice == prem1.getId()) {
             if (prem1.isAvailable()) {
                 prem1.setAvailable(false);
                 IO.println(client.getName() + ", you rented: " + prem1);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
         } else if (carChoice == prem2.getId()) {
             if (prem2.isAvailable()) {
                 prem2.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + prem2);
+                System.out.println(client.getName() + ", you rented: " + prem2);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
         } else if (carChoice == prem3.getId()) {
             if (prem3.isAvailable()) {
                 prem3.setAvailable(false);
-                IO.println(client.getName() + ", you rented: " + prem3);
+                System.out.println(client.getName() + ", you rented: " + prem3);
             } else {
-                IO.println("This car is already rented");
+                System.out.println("This car is already rented");
             }
-        }
     } else {
-        count = 1;
-        IO.println("Car not found");
+        System.out.println("Car not found");
     }
 }
 
@@ -151,11 +148,11 @@ public static void processRent(Vehicle vehicle, Client client, int count) {
     if (count == 0) {
         if (vehicle.isAvailable()) {
             vehicle.setAvailable(false);
-            IO.println(client.getName() + ", you rented: " + vehicle);
+            System.out.println(client.getName() + ", you rented: " + vehicle);
         } else {
-            IO.println("This car is already rented");
+            System.out.println("This car is already rented");
         }
     } else {
-        IO.println("This car not this category");
+        System.out.println("This car not this category");
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -4,15 +4,17 @@ public class Vehicle {
     private final String model;
     private final int price;
     private final String category;
+    private final int category_id;
     private boolean available;
 
-    public Vehicle(int car_id, String brand, String model, int price, String category) {
+    public Vehicle(int car_id, String brand, String model, int price, String category, int category_id) {
         this.car_id = car_id;
         this.brand = brand;
         this.model = model;
         this.price = price;
         this.category = category;
         this.available = true;
+        this.category_id = category_id;
     }
 
     public int getId() { return car_id; }
@@ -24,7 +26,7 @@ public class Vehicle {
 
     @Override
     public String toString() {
-        return "Vehicle [id=" + car_id + ", Brand=" + brand + ", Model=" + model +
-                ", Price=" + price + "(Price = 1-day)" + "]";
+        return "Vehicle [id = " + car_id + ", category = " + category + ", Brand = " + brand + ", Model = " + model +
+                ", Price = " + price + "]";
     }
 }
